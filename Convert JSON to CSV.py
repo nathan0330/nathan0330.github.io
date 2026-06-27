e6075ba7c88646011f21e9965e8f12dea6fadfdf
nathan0330 <nathanhanpiper@gmail.com>
2026-06-27 15:07:32 -0500

    Update Convert JSON to CSV.py

--- Convert JSON to CSV.py
+++ Convert JSON to CSV.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import json
 
-path = "/Users/nathanhan/Desktop/failure-mileage-distribution.json"
+path = "/Users/nathanhan/Desktop/failure-mileage-distribution.json"   #------ replace the pasth name accordingly 
 
 def read_json(file_path):
     with open(file_path) as f:
@@ -13,7 +13,7 @@ def read_json(file_path):
 def json_to_csv(file):
     items = file["rows"]
     df = pd.DataFrame(items)
-    df.to_csv(os.path.join("/Users/nathanhan/Desktop", "failure-mileage-distribution.csv"), index=False)
+    df.to_csv(os.path.join("/Users/nathanhan/Desktop", "failure-mileage-distribution.csv"), index=False)     #---- replace the destination path accordingly
     print("Successfully saved failure-mileage-distribution.csv")
 
 
